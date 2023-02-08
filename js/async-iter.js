f062d8870040953d14ca000034f2d4dcb3e57a95
Kevin Haas <kevin.haas96@gmail.com>
2023-02-08 15:59:48 -0500

    Update async-iter.js

--- js/async-iter.js
+++ js/async-iter.js
@@ -1,3 +1,17 @@
+/**
+ * A function that will iterate over an array and yield the value of each item in the array.
+ *  
+ * @param {Array} arr The array to iterate over.
+ *  
+ * @returns {Promise} A promise that will resolve with the value of the current item in the array.
+ *  
+ * @example
+ *  
+ * var someItems = [1, 2, 3];
+ * for await (let item of iter(someItems)) {
+ *    console.log(item);
+ * }
+ */
 function* iter(arr) {
     let i = 0;
     while (i < arr.length) {
