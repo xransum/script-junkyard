98b6210e9b4b731654fb69ea8cff9ed107005dbd
Kevin Haas <kevin.haas96@gmail.com>
2023-02-08 16:02:19 -0500

    Update async-sleep.js

--- js/async-sleep.js
+++ js/async-sleep.js
@@ -1,7 +1,20 @@
+/**
+ * A function that will pause the execution of the script for the 
+ * amount of milliseconds specified.
+ *  
+ * @param {number} ms The amount of milliseconds to pause the script for.
+ *  
+ * @returns {Promise} A promise that will resolve after the specified amount of time.
+ *  
+ * @example
+ *  
+ * await sleep(2000);
+ */
 function sleep(ms) {
-    // await sleep(2000);
-    // return await for better async stack support in case of errors.
+    // Returns a promise to allow for better async stack support in case of errors.
     return new Promise((resolve) => {
-		setTimeout(resolve, ms);
-	});
+        // setTimeout will call the resolve after the specified 
+        // amount of time.
+        setTimeout(resolve, ms);
+    });
 }
