0a779d0699e248d7f10676fc9b26105c997914a3
Kevin Haas <kevin.haas96@gmail.com>
2023-02-22 12:50:18 -0500

    Update to-id.js

--- js/to-id.js
+++ js/to-id.js
@@ -1,3 +1,15 @@
+/**
+ * A function that will take a string and convert it to a valid lowercase id.
+ * 
+ * @param {String} str The string to convert.
+ * 
+ * @returns {String} The converted string.
+ * 
+ * @example
+ * 
+ * stringToId('Hello World');
+ * // hello-world
+ */
 function stringToId(text) {
     let str = text;
     return str.toLowerCase().replace(/\W+/g, '-').replace(/\-{2,}/g, '-').replace(/^\-+|\-+$/g, '');
