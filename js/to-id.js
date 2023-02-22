2f38874f7fe6d856df3cd4f95a8e1bda7cca0d49
Kevin Haas <kevin.haas96@gmail.com>
2023-02-22 12:47:54 -0500

    Update to-id.js

--- js/to-id.js
+++ js/to-id.js
@@ -1,4 +1,4 @@
-function toId(text) {
+function stringToId(text) {
     let str = text;
-    return str.toLowerCase().replace(/\W+/g, '-');
+    return str.toLowerCase().replace(/\W+/g, '-').replace(/\-{2,}/g, '-').replace(/^\-+|\-+$/g, '');
 }
