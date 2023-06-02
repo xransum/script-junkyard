68a9f37ded38d5a4d105cfd434281afe1b8893e6
Kevin Haas <kevin.haas96@gmail.com>
2023-06-02 09:39:14 -0400

    Update triggerEvent.js

--- js/triggerEvent.js
+++ js/triggerEvent.js
@@ -1,3 +1,9 @@
+/**
+ * Triggers an event on the element
+ * 
+ * @param {string} eventName
+ * @returns {HTMLElement}
+ */
 function triggerEvent(eventName) {
     if (document.createEvent) {
         this.dispatchEvent(new Event(eventName));
