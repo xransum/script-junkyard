d8a54d01ad6d221bff0b1d47569ecc2700aecc45
Kevin Haas <29680216+xransum@users.noreply.github.com>
2024-01-10 10:28:09 -0500

    Update delay.js

--- js/delay.js
+++ js/delay.js
@@ -1,17 +1,28 @@
-/**
- * Delay function call until after a certain amount of 
- * time has passed since the last time it was called.
+/** Delayed function execution.
  * 
- * @param {function} callback
- * @param {number} ms
- * @return {function}
- */
+ * When called consecutively, this function will delay the execution
+ * of the callback function until the delay time has passed. This is
+ * useful for functions that are called multiple times in a short
+ * period of time, such as when a user is typing in a text box.
+ * 
+ * @author xransum <https://github.com/xransum>
+ * @see https://github.com/xransum/script-junkyard/blob/main/js/delay.js
+ * 
+ * @param {function} callback The function to execute.
+ * @param {number} ms The number of milliseconds to delay.
+ * @returns {function} The callback function.
+ * 
+ * @example
+ * var delayedFunction = delay(function() {
+ *    console.log('Hello World!');
+ * }, 1000);
+ **/
 function delay(callback, ms) {
     var timer = 0;
     return function() {
         var context = this;
         var args = arguments;
-        
+
         clearTimeout(timer);
         timer = setTimeout(function() {
             callback.apply(context, args);
