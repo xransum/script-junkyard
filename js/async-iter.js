f600814c7f9af5fc71fb6a4ab487be541d492b9b
Kevin Haas <29680216+xransum@users.noreply.github.com>
2024-01-24 12:22:16 -0500

    Update async-iter.js

--- js/async-iter.js
+++ js/async-iter.js
@@ -1,15 +1,18 @@
 /**
- * A function that will iterate over an array and yield the value of each item in the array.
- *  
- * @param {Array} arr The array to iterate over.
- * @returns {Promise} A promise that will resolve with the value of the current item in the array.
- *  
+ * A function that iterates over an array and yields the value of each item in the array.
+ *
+ * @link       https://github.com/xransum/script-junkyard/blob/main/js/async-iter.js
+ * @global
+ *
+ * @param {Array} arr - The array to iterate over.
+ * @yield {Promise} A promise that will resolve with the value of the current item in the array.
+ *
  * @example
- *  
  * var someItems = [1, 2, 3];
  * for await (let item of iter(someItems)) {
  *    console.log(item);
  * }
+ *
  */
 function* iter(arr) {
     let i = 0;
