54cad6e0a85b0660b0b41da02794151d069943f5
Kevin Haas <29680216+xransum@users.noreply.github.com>
2024-01-24 12:25:00 -0500

    Update async-sleep.js

--- js/async-sleep.js
+++ js/async-sleep.js
@@ -1,17 +1,19 @@
 /**
- * A function that will pause the execution of the script for the 
- * amount of milliseconds specified.
- *  
- * @param {number} ms The amount of milliseconds to pause the script for.
- *  
+ * A function that pauses the execution of the script for the specified amount of milliseconds.
+ *
+ * @link       https://github.com/xransum/script-junkyard/blob/main/js/async-sleep.js
+ * @global
+ *
+ * @param {number} ms - The amount of milliseconds to pause the script for.
  * @returns {Promise} A promise that will resolve after the specified amount of time.
- *  
+ *
  * @example
- *  
  * await sleep(2000);
+ *
  */
-function sleep(ms) {
-    // Returns a promise to allow for better async stack support in case of errors.
+function* sleep(ms) {
+    // Returns a promise to allow for better async stack support in
+    // case of errors.
     return new Promise((resolve) => {
         // setTimeout will call the resolve after the specified 
         // amount of time.
